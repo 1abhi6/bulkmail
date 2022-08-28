a27008b39fd71cb3f1d1a77039255d80a5a6379e
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-08-28 22:00:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+# Importing required modules
+
+from asyncio import constants
 import pandas as pd
 import smtplib as smt
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from getpass import getpass
 
-print("Reading mails from Excel")
-data = pd.read_excel("info.xlsx")
-emailData = data.get("Email")
-emailList = list(emailData)
 
 try:
+    # Reading mails from excel file
+    print("Reading mails from Excel")
+    data = pd.read_excel("info.xlsx")
+    emailData = data.get("Email")
+    emailList = list(emailData)
+
+    # Reading message from HTML file
+    HTMLFile = open("index.html", "r")
+    messageHTML = HTMLFile.read()
+
     # SMTP Object
     print("In process...")
     mailServer = smt.SMTP("smtp.gmail.com", 587)
-    mailServer.starttls()
+    mailServer.starttls()  # Starting the server
 
-    # Inputting email and password
+    # Inputting Email and Password
     senderMail = input("Enter your Email address: ")
     senderPass = getpass("Input your password: ")
-    # login via email
-    mailServer.login(senderMail, senderPass)
-    print("Login Sucessful")
+
     # Setting up the email subject, to, from, and message
     fromEmail = senderMail
     toEmail = emailList
@@ -28,31 +35,22 @@ try:
     Emailmessage['Subject'] = "Testing for Python Project"
     Emailmessage['from'] = senderMail
 
-    # Creating the text via HTML
-    messageHTML = '''
-    <html>
-        <head>
-            <style>
-                .btn{
-                  background:red;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>HEading 1</h1>
-            <button id="btn">Test</button>
-        </body>
-    </html>
-    '''
+    # Login via email
+    mailServer.login(senderMail, senderPass)
+    print("Login Sucessful")
 
     # Attach the message and Multipart
     textMsg = MIMEText(messageHTML, "html")
     Emailmessage.attach(textMsg)
 
     # Sending the email
-    mailServer.sendmail(fromEmail, toEmail, Emailmessage.as_string)
-    for email in emailList:
-        print("Message has been sent to ", emailList[email])
+    mailServer.sendmail(fromEmail, toEmail, Emailmessage.as_string())
+    mailServer.quit()
+
+    # Displaying the successful message
+    for email in range(len(emailList)):
+        print("Message has been sent to", emailList[email])
+
 
 except Exception as error:
     print(error)
