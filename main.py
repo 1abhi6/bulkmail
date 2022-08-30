cc0ed274f2f54729c6e92adc983893ffb6a5a9f8
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-08-30 16:48:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import smtplib as smt
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from getpass import getpass
+import re
+
+# Regular expression for validating the mail
+regex = re.compile(
+    r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+# Function to validate the mail using Regex module
+
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+        return email
 
 
 try:
@@ -14,6 +26,19 @@ try:
     data = pd.read_excel("info.xlsx")
     emailData = data.get("Email")
     emailList = list(emailData)
+
+    # emailList contains all the mails read from the excel file
+    # Now, we will check each mail using isValid function
+    validEmail = map(isValid, emailList)
+    validEmailListUnFiltered = list(validEmail)
+
+    # validEmailListUnFiltered returns the valid mails as well as invalid mails as none in a list
+    # To filter all the valid mails we will use filter() function
+    validEmailListFiltered = filter(
+        lambda emailValue: emailValue != None, validEmailListUnFiltered)
+    listOfFinalEmails = list(validEmailListFiltered)
+
+    print("Mail will be send of these emails:", listOfFinalEmails)
 
     # Reading message from HTML file
     HTMLFile = open("index.html", "r")
@@ -30,14 +55,14 @@ try:
 
     # Setting up the email subject, to, from, and message
     fromEmail = senderMail
-    toEmail = emailList
+    toEmail = listOfFinalEmails
     Emailmessage = MIMEMultipart("alternative")
     Emailmessage['Subject'] = "Testing for Python Project"
     Emailmessage['from'] = senderMail
 
     # Login via email
     mailServer.login(senderMail, senderPass)
-    print("Login Sucessful")
+    print("Login Successful")
 
     # Attach the message and Multipart
     textMsg = MIMEText(messageHTML, "html")
@@ -48,8 +73,8 @@ try:
     mailServer.quit()
 
     # Displaying the successful message
-    for email in range(len(emailList)):
-        print("Message has been sent to", emailList[email])
+    for email in range(len(listOfFinalEmails)):
+        print("Message has been sent to", listOfFinalEmails[email])
 
 
 except Exception as error:
