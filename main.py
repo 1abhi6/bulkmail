b3194cf7f3d0cb849379f59ff086fe066b6c8438
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-09-07 12:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import re
 from tkinter import *
+from tkinter import messagebox
 
 # Regular expression for validating the mail
 regex = re.compile(
@@ -54,10 +55,6 @@ def sendEmail():
         mailServer = smt.SMTP("smtp.gmail.com", 587)
         mailServer.starttls()  # Starting the server
 
-        # Inputting Email and Password
-        # senderMail = input("Enter your Email address: ")
-        # senderPass = getpass("Input your password: ")
-
         # Setting up the email subject, to, from, and message
         fromEmail = senderMail
         toEmail = listOfFinalEmails
@@ -82,23 +79,23 @@ def sendEmail():
         else:
             return False
 
-        # Displaying the successful message
-        # for email in range(len(listOfFinalEmails)):
-        #     print("Message has been sent to", listOfFinalEmails[email])
-        # return True
     except Exception as error:
         print(error)
 
+
 # Creating GUI
 window = Tk()
-window.geometry("500x350")
+window.geometry("700x500")
 windowIcon = window.iconbitmap("icon.ico")
-# Setting icon of master window
-# window.iconphoto(False, windowIcon)
+window.title("Bulk mail sender ")
+# Set the window color
+window.configure(bg='#bdc3c7')
 
-# This function will get the  sender's email credentials from GUI
+# This function will get the  sender's email credentials with the help of GUI
+
+
 def getEmailData():
-    
+
     global senderMail
     senderMail = email.get()
     global senderPass
@@ -109,27 +106,40 @@ def getEmailData():
     # If mail is sent successfully if block will execute
     if(retsendEmail):
         dispMsg = readMails()
-        Label(window, text="Email Sent Successfully!!!",
-              font=('Arial 12')).place(x=60, y=190)
+        messagebox.showinfo(
+            "Success", "Email Sent Successfully!!!")
         Label(window, text="Message has been sent to: ",
-              font=('Arial 12')).place(x=60, y=220)
-        posy = 0
+              font=('Arial 12'), bg="#bdc3c7").place(x=60, y=220)
+
+        # Create listbox object
+        pos = 0
+        listbox = Listbox(window, height=10,
+                          width=30,
+                          bg="#ecf0f1",
+                          activestyle='dotbox',
+                          font="Arial"
+                          )
+        # Looping over the list of successfully sent emails
         for i in range(len(dispMsg)):
-            Label(window, text=str(dispMsg[i]+"\n"),
-                  font=('Arial 12')).place(x=60, y=245+posy)
-            posy = posy+25
+            listbox.insert(++pos, str(dispMsg[i]))
+
+        listbox.place(x=60, y=260)
+
     # If mail is not sent
     else:
-        Label(window, text="Not sent, Due to invalid login credentials.",
-              font=('Arial 12')).place(x=60, y=190)
-    
+        messagebox.showerror(
+            "Error", "Not sent, Due to invalid login credentials.")
+
+
 #  Create widgets
 email = StringVar()
 passw = StringVar()
 
 # Label widget for email and password
-emailLabel = Label(window, text="Email", font=('Arial 12')).place(x=30, y=50)
-password = Label(window, text="Password", font=('Arial 12')).place(x=30, y=90)
+emailLabel = Label(window, text="Email", bg="#bdc3c7",
+                   font=('Arial 12')).place(x=30, y=50)
+password = Label(window, text="Password", bg="#bdc3c7",
+                 font=('Arial 12')).place(x=30, y=90)
 
 # Entry widget field for email and password
 entry_variable = Entry(window, textvariable=email, width=20,
@@ -138,7 +148,7 @@ entry_variable = Entry(window, textvariable=passw, width=20,
                        font=('Arial 12'), show="*").place(x=120, y=90)
 
 # Button widget to send the email
-button_submit = Button(window, text="Send Email", font=('Arial 12'), padx=20,bg='#ffffff', activebackground='#00ff00', command=getEmailData).place(
+button_submit = Button(window, text="Send Email", font=('Arial 12'), padx=20, bg="#ecf0f1", activebackground='#e6f2f2', command=getEmailData).place(
     x=130, y=140
 )
 window.mainloop()
